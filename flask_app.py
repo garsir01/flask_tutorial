315e9eafef95fe52f5533f14c9201f5b8301878c
garsir01 <robert.garside@bbc.co.uk>
2019-04-30 16:30:15 +0100

    tever

--- flask_app.py
+++ flask_app.py
@@ -5,19 +5,40 @@ import pandas as pd
 import numpy as np
 from flask import request
 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+i=1
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    __tablename__ = "CUSTOM_TABLE_NAME"
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @app.route("/")
 def hello():
+	
+	api_request = User(id=i, username="bob", email="bob@gmail.com")
+	db.session.add(api_request)
+	db.session.commit()
 	return "f u"
-@app.route("/api", methods=["POST"])
+
+@app.route("/api", methods=["GET","POST"])
 def outputdata():
-	data = {'me':'you'}
-	data = request.get_json(force=True)
-	print(data)	
-	return jsonify(data)
+    if request.method == "GET":
+        return str(jsonify(data))
+	
+
+    data = request.get_json(force=True)
+    print(data)
+    return jsonify(data)
 
 
    

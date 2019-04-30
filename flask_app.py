018f5f3ba45c950dbbafe7d54a1780c7a40df7f3
garsir01 <robert.garside@bbc.co.uk>
2019-04-30 11:50:40 +0100

    new3

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,16 @@
 # flask_app.py
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route("/")
 def hello():
     return "f u"
+@app.route("/api")
+def outputdata():
+
+
+
+
+    jsonify(data)

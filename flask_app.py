3bed543944b762d73b39405e20eb2899dd3624b3
garsir01 <robert.garside@bbc.co.uk>
2019-04-30 12:15:26 +0100

    lol

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,16 @@
 # flask_app.py
 from flask import Flask
 from flask import jsonify
+import pandas as pd
+import numpy as np
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route("/")
 def hello():
-    return "f u"
+	return "f u"
 @app.route("/api")
 def outputdata():
-
-
-
-
-    jsonify(data)
+	data = pd.DataFrame(np.random.randn(10, 4), columns=list('ABCD'))
+	return data.to_json()
